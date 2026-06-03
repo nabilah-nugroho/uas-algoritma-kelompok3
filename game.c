fc0df2f4afc584adf6c7ba2b18dc1ecdc4d2cc25
khairistakemi <kimikukimi06@gmail.com>
2026-06-03 09:50:35 +0700

    fix: menambahkan int main

--- game.c
+++ game.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+
+int main() {
 printf ("1. Tambah Hadiah\n");
 printf ("2. Tamabah Gerak O\n");
 printf ("3. Simulasikan Lite O\n");
 printf ("4. Keluar\n");
 printf ("Masukkan Menu: ");
+}
