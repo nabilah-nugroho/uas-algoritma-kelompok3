5f3a5fac35fdf6dc3ad246aba08c341c121b88ca
nabilah nugroho <boredluckykid@gmail.com>
2026-06-04 10:36:13 +0700

    style: merapikan file

--- game.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-printf ("1. Tambah Hadiah\n");
-printf ("2. Tamabah Gerak O\n");
-printf ("3. Simulasikan Lite O\n");
-printf ("4. Keluar\n");
-printf ("Masukkan Menu: ");
-}
