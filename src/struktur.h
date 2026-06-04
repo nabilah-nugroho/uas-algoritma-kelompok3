4b54ac72a8c32cbee534bd4d89ad661a11422c0c
nabilah nugroho <boredluckykid@gmail.com>
2026-06-04 10:57:28 +0700

    feat: menambahkan struct dan edit file struktur.h

---
+++ src/struktur.h
@@ -0,0 +1,24 @@
+// struktur.h
+#ifndef STRUKTUR_H
+#define STRUKTUR_H
+
+
+// struct data hadiah
+typedef struct {
+    int x;
+    int y;
+    char nama[20];
+    int skor;
+} Hadiah;
+
+// struct gerakan o
+typedef struct {
+    int x;
+    int y;
+} Gerak;
+
+extern int skorTotal;       //menampung skor si O selama simulasi
+extern int panjangPapan;    //input tinggi papan dari user
+extern int lebarPapan;      //input lebar papan dari user
+
+#endif
