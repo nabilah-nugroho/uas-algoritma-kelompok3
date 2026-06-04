f3cccfc0a04cd4888ed86d1f71de8becf180244e
unknown <diyaraditya07@gmail.com>
2026-06-04 11:38:29 +0700

    feat membuat kode sorting

---
+++ src/sorting.c
@@ -0,0 +1,31 @@
+#include <struktur.h>
+
+int lebihBesar(Hadiah a, Hadiah b)
+{
+    if (a.y > b.y)
+    {
+    return 1;
+    }
+    if (a.y == b.y && a.x > b.x)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+void sortHadiah(Hadiah data[], int jumlah)
+{
+    int i, j;
+    Hadiah temp;
+    for (i = 1; i < jumlah; i++)
+    {
+        temp = data[i];
+        j = i - 1;
+        while (j >= 0 && lebihBesar(data[j], temp))
+        {
+            data[j + 1] = data[j];
+            j--;
+        }
+        data[j + 1] = temp;
+    }
+}
