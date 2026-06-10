9e55753d61e6219be97c8478719e864eec047c0c
nabilah nugroho <boredluckykid@gmail.com>
2026-06-10 09:01:47 +0700

    feat: menambahkan fitur baca gerak

--- src/arsip.c
+++ src/arsip.c
@@ -28,3 +28,57 @@ void bacaHadiah(Hadiah data[], int *jumlah) { //pakek pointer supaya bs dikirim 
     fclose(fp); //nutup file
 }
 
+void bacaGerak(Gerak data[], int *jumlah) {
+    //buka file rute jalan O di folder data dengan mode cuma baca (read)
+    FILE *fp = fopen("data/tgerak.txt", "r"); 
+    if (fp == NULL) {
+        *jumlah = 0;
+        return;
+    }
+
+    char token[20]; 
+    int i = 0;     
+
+    while (fscanf(fp, "%s", token) != EOF) {
+        //cek sentinel: kalau nemu "##" di file tgerak.txt, loop langsung berhenti
+        if (strcmp(token, "##") == 0) {
+            break;
+        }
+
+        //kalau bukan "##", ubah teks angka tadi jadi integer koordinat X gerak O
+        data[i].x = atoi(token);
+        
+        //krena file gerak cuma butuh koordinat X dan Y, fscanf berikutnya
+        // cuma nyomot 1 angka di sebelahnya buat koordinat Y gerak O
+        fscanf(fp, "%d", &data[i].y);
+        
+        i++; // Naik ke indeks berikutnya buat langkah jalan O selanjutnya
+    }
+
+    *jumlah = i; // Kasih tahu main.c total langkah yang bakal dilewati si O
+    fclose(fp);  // Tutup file
+}
+
+void bacaGerak(Gerak data[], int *jumlah) {
+    FILE *fp = fopen("data/tgerak.txt", "r");
+    if (fp == NULL) {
+        *jumlah = 0;
+        return;
+    }
+
+    char teks[20];
+    int i = 0;
+
+    while (fscanf(fp, "%s", teks) != EOF) {
+        if (strcmp(teks, "##") == 0) {
+            break;
+        }
+
+        data[i].x = atoi(teks);
+        fscanf(fp, "%d", &data[i].y);
+        i++;
+    }
+
+    *jumlah = i;
+    fclose(fp);
+}
