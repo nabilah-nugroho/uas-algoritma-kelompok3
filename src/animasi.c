2b393290245cd1375604e95be559ebdf021e5e48
arkaansyukur-dev <syukurarkaan@gmail.com>
2026-06-10 09:26:35 +0700

    feat: mencoba simulasi gerak

--- src/animasi.c
+++ src/animasi.c
@@ -10,3 +10,27 @@ void wait(float x) {
     while (difftime(current,start) < x);
 }
 
+void simulasiGerak() {
+    FILE *f;
+    gerak G;
+
+    f = fopen("tgerak.txt", "r");
+
+    if (f != NULL) {
+        while (fscanf(f, "%d %d", &G.x, &G.y) != EOF) {
+
+            system("cls");
+
+            // update posisi O
+            posisiO.x = G.x;
+            posisiO.y = G.y;
+
+            // tampilkan papan
+            tampilPapan();
+
+            wait(1);
+        }
+
+        fclose(f);
+    }
+}
