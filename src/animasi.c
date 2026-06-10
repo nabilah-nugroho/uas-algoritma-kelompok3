f3df1ee9afc6957f5577ec63d051e7a9f6400037
arkaansyukur-dev <syukurarkaan@gmail.com>
2026-06-10 08:54:03 +0700

    feat: menambahkan fitur wait

--- src/animasi.c
+++ src/animasi.c
@@ -1,1 +1,11 @@
-//
+#include "struktur.h"
+#include <time.h>
+
+void wait(float x) {
+    time_t start;
+    time_t current;
+    time(&start);
+    do
+        time(&current);
+    while (difftime(current,start) < x);
+}
