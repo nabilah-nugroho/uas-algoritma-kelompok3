18046bb0cfbbb6e7a742debc61e062beb2c6b375
khairistakemi <kimikukimi06@gmail.com>
2026-06-18 16:23:01 +0700

    fix: menyesuaikan variabel

--- src/skor.c
+++ src/skor.c
@@ -1,26 +1,21 @@
-#include <stdio.h>
 #include "struktur.h"
+#include <stdio.h>
 
-int skorTotal = 0; 
-int panjangPapan = 0;
-int lebarPapan = 0;
+int skorTotal; 
 
-void cekTabrakan(Gerak posisiOSekarang, Hadiah daftarHadiah[], int jumlahHadiah) {
-    
-    // Cek semua hadiah satu per satu
+extern Hadiah daftarHadiah[]; 
+extern int jumlahHadiah;
+
+void cekHadiah(int x, int y) {
     for (int i = 0; i < jumlahHadiah; i++) {
-        
-        // Cek jika koordinat sama DAN hadiahnya masih ada
-        if (posisiOSekarang.x == daftarHadiah[i].x && posisiOSekarang.y == daftarHadiah[i].y && daftarHadiah[i].skor > 0) {
-            
-            // Tambah skor total dengan skor hadiah
-            skorTotal = skorTotal + daftarHadiah[i].skor;
-            
-            // Set skor jadi 0 agar hadiah hilang dari layar
-            daftarHadiah[i].skor = 0;
-            
-            // Berhenti cek karena hadiah sudah dimakan
-            break;
+        if (daftarHadiah[i].x == x && daftarHadiah[i].y == y) {
+
+            skorTotal += daftarHadiah[i].skor;
+
+            daftarHadiah[i].x = -1;
+            daftarHadiah[i].y = -1;
+
+            printf("\nHadiah '%s' dimakan! Skor sekarang: %d\n", daftarHadiah[i].nama, skorTotal);
         }
     }
 }
